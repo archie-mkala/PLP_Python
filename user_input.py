60b181e2385a48e2b95f13e2ca68d96e6d7616d7
Archie Mkala <archieapollo6@gmail.com>
2024-10-31 10:53:50 +0300

    assignment done

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input('Enter Your Name: ')
+age = input('Enter Your Age: ')
+location = input('Enter Your location: ')
+
+print(f'Hello {name}, You are {age} years old and you live in {location}')
